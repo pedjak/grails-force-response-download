61942d541703b2f0b385967318cfaac19a932855
Predrag Knezevic <predrag.knezevic@abz-reporting.com>
2011-07-15 09:22:23 +0200

    allow using browserDetection plugin with version 0.1 and above

--- ForceResponseDownloadGrailsPlugin.groovy
+++ ForceResponseDownloadGrailsPlugin.groovy
@@ -1,10 +1,10 @@
 class ForceResponseDownloadGrailsPlugin {
     // the plugin version
-    def version = "0.1.1"
+    def version = "0.1.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ["browserDetection":'0.1']
+    def dependsOn = ["browserDetection":'0.1 > *']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
